6aa6466d7350896818abbdd5118464300f2d826d
Tamajit123 <85442393+Tamajit123@users.noreply.github.com>
2022-08-03 13:02:32 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (31.34%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.cpp
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -1,3 +1,4 @@
+#define vi vector<int>
 class Solution {
 public:
     int solveMem(int n,vector<int>&dp){
@@ -11,8 +12,21 @@ public:
         dp[n] = solveMem(n-1,dp) + solveMem(n-2,dp);
         return dp[n];
     }
+    int solveTab(int n){
+        vi dp(n+1,0);
+        
+        for(int i = 0; i<=n; i++){
+            if(i<= 1){
+                dp[i] = 1;
+                continue;
+            }
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        return dp[n];
+    }
     int climbStairs(int n) {
-        vector<int> dp(n+1, -1);
-        return solveMem(n,dp);
+        // vector<int> dp(n+1, -1);
+        // return solveMem(n,dp);
+        return solveTab(n);
     }
 };

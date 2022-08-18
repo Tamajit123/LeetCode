1eaf49f9bf0d6abc17ef71e3fd435ec539c96182
Tamajit123 <85442393+Tamajit123@users.noreply.github.com>
2022-08-18 13:00:04 +0530

    Time: 41 ms (48.06%), Space: 20.8 MB (35.45%) - LeetHub

--- 1402-reducing-dishes/1402-reducing-dishes.cpp
+++ 1402-reducing-dishes/1402-reducing-dishes.cpp
@@ -18,12 +18,28 @@ public:
         
         return dp[index][time];
     }
+    int solveTab(vector<int>& satisfaction){
+        int n = satisfaction.size();
+        vector<vector<int>> dp(n+1,vector<int>(n+1,0));
+        
+        
+        for(int index= n-1; index>=0; index-- ){
+            for(int time = index; time >=0 ; time --){
+                int incld = satisfaction[index]*(time+1) + dp[index+1][time + 1]; 
+                int excld = 0+ dp[index+1][time];
+                dp[index][time] = max(incld, excld);
+            }
+        }
+        return dp[0][0]  ;
+    }
     int maxSatisfaction(vector<int>& satisfaction) {
         
         sort(satisfaction.begin() , satisfaction.end());
         
         int n = satisfaction.size();
-        vector<vector<int>> dp(n+1,vector<int>(n+1,-1));
-        return solveMem(satisfaction,0,0,dp);
+        // vector<vector<int>> dp(n+1,vector<int>(n+1,-1));
+        // return solveMem(satisfaction,0,0,dp);
+        
+        return solveTab(satisfaction);
     }
 };

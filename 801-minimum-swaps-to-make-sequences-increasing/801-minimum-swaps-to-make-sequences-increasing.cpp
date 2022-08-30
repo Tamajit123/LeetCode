85f78734418e8e2cb6cbd9b986f3fc021cc938ce
Tamajit123 <85442393+Tamajit123@users.noreply.github.com>
2022-08-30 13:25:29 +0530

    Time: 297 ms (75.09%), Space: 120.3 MB (53.25%) - LeetHub

--- 801-minimum-swaps-to-make-sequences-increasing/801-minimum-swaps-to-make-sequences-increasing.cpp
+++ 801-minimum-swaps-to-make-sequences-increasing/801-minimum-swaps-to-make-sequences-increasing.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     int solve(vector<int>& nums1, vector<int>& nums2, int index, bool isSwap,vector<vector<int>> &dp){
+        
         if(index == nums1.size())
             return 0;
         
@@ -24,6 +25,40 @@ public:
         
         return dp[index][isSwap] = ans;
     }
+    
+    
+    int solveTab(vector<int>& nums1, vector<int>& nums2){
+        int n = nums1.size();
+        vector<vector<int>>dp(n+1,vector<int>(2, 0));
+        
+        // if(index == nums1.size())
+        //     return 0;
+
+        
+        
+        for(int index = n-1; index >=1; index--){
+            for(int isSwap = 1; isSwap >= 0; isSwap --){
+                
+                int prev1 = nums1[index-1];
+                int prev2 = nums2[index-1];
+        
+                if(isSwap)
+                    swap(prev1, prev2);
+        
+                int ans  = INT_MAX;
+                
+                if(nums1[index] > prev1 and nums2[index] > prev2)
+                    ans = dp[index+1][0];
+                if(nums1[index] > prev2 and nums2[index] > prev1)
+                    ans = min(ans, 1+dp[index+1][1]);
+                
+                dp[index][isSwap] = ans;
+            }
+        }
+        
+        return dp[1][0];
+        
+    }
     int minSwap(vector<int>& nums1, vector<int>& nums2) {
         
         nums1.insert(nums1.begin(), -1);
@@ -31,7 +66,9 @@ public:
         
         bool isSwap = 0;
         int n= nums1.size();
-        vector<vector<int>>dp(n ,vector<int>(2, -1));
-        return solve(nums1, nums2, 1,isSwap,dp);
+        // vector<vector<int>>dp(n ,vector<int>(2, -1));
+        // return solve(nums1, nums2, 1,isSwap,dp);
+        
+        return solveTab(nums1,nums2);
     }
 };

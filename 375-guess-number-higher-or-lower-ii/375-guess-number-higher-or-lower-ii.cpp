641601bad475ca4e06814b69f2dfe32c7eacf8d9
Tamajit123 <85442393+Tamajit123@users.noreply.github.com>
2022-09-03 04:00:28 +0530

    Time: 229 ms (53.43%), Space: 8.3 MB (8.27%) - LeetHub

--- 375-guess-number-higher-or-lower-ii/375-guess-number-higher-or-lower-ii.cpp
+++ 375-guess-number-higher-or-lower-ii/375-guess-number-higher-or-lower-ii.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     int solveMem(int start, int end, vector<vector<int>> &dp){
+        
         if(start >= end)
             return 0;
         
@@ -15,8 +16,30 @@ public:
         return dp[start][end] = mini;
     }
     
+    int solveTab(int n){      
+        
+        vector<vector<int>> dp(n+2,vector<int>(n+2,0));
+        
+        for(int start = n; start >= 1 ; start--){
+            for(int end = start; end <= n; end++){
+                if(start == end)
+                    continue;
+                else{
+                    int mini = INT_MAX;
+                    for(int i = start; i<= end; i++){
+                       mini = min(mini, i+ max(dp[start][i-1],dp[i+1][end]));
+                   }
+                dp[start][end] = mini;
+            }
+          }
+        }
+        return dp[1][n];
+    }
+    
     int getMoneyAmount(int n) {
-        vector<vector<int>> dp(n+1,vector<int>(n+1,-1));
-        return solveMem(1,n,dp);
+        // vector<vector<int>> dp(n+1,vector<int>(n+1,-1));
+        // return solveMem(1,n,dp);
+        
+        return solveTab(n);
     }
 };

5335b07121bfa1912d2dfc22c569ff5953f62cdb
Tamajit123 <85442393+Tamajit123@users.noreply.github.com>
2022-09-03 03:13:47 +0530

    Time: 0 ms (100.00%), Space: 6 MB (39.13%) - LeetHub

--- 96-unique-binary-search-trees/96-unique-binary-search-trees.cpp
+++ 96-unique-binary-search-trees/96-unique-binary-search-trees.cpp
@@ -14,9 +14,25 @@ public:
         }
         return dp[n] = ans;
     }
+    int solveTab(int n){
+        
+        vector<int>dp(n+1,0);
+        dp[0] = dp[1] = 1;
+
+        
+        // i= number of nodes
+        for(int i = 2; i<=n ; i++){
+            //root nodes
+            for(int j = 1; j<=i; j++){
+                dp[i] += dp[j-1] * dp[i-j]; 
+            }
+        }
+        return dp[n];
+    }
     
     int numTrees(int n) {
-        vector<int> dp(n+1,-1);
-        return solveMem(n, dp);
+        // vector<int> dp(n+1,-1);
+        // return solveMem(n, dp);
+        return solveTab(n);
     }
 };

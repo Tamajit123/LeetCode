c5310eea7e6078e46d29075574fc91324ecd9f1f
Tamajit123 <85442393+Tamajit123@users.noreply.github.com>
2022-09-05 13:00:09 +0530

    Time: 10 ms (66.34%), Space: 13.1 MB (29.28%) - LeetHub

--- 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -49,11 +49,42 @@ public:
         return dp[0][1];
     }
     
+    int solveSO(vector<int>& prices){
+        
+        int n = prices.size();
+        vector<int> curr(2, 0);
+        vector<int> next(2,0);
+        
+        //vector<vector<int> >dp(n+1, vector<int>(2,0));
+
+    int maxProfit = 0;
+      
+        for(int index = n-1; index>=0; index--){
+            for(int buy = 0;buy <=1; buy++){
+                
+        if(buy){
+            int isBuy = -prices[index] + next[0];
+            int notBuy = 0 + next[1];
+            maxProfit = max(isBuy, notBuy );
+        }
+        
+        else{
+            maxProfit = max( (prices[index] + next[1]) , (0+ next[0]) );
+        }
+                curr[buy] = maxProfit;
+                
+            }
+            next = curr;
+        }
+        
+        return next[1];
+    }
+    
 
     int maxProfit(vector<int>& prices) {
         // int n = prices.size();
         // vector<vector<int> >dp(n, vector<int>(2,-1));
         // return solve(0, 1, prices,dp);
-        return solveTab(prices);
+        return solveSO(prices);
     }
 };

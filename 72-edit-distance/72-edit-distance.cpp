6fd3e30dca9202d4ef40bd01a185a82976c755db
Tamajit123 <85442393+Tamajit123@users.noreply.github.com>
2022-09-06 12:06:26 +0530

    Time: 26 ms (42.85%), Space: 6.5 MB (94.40%) - LeetHub

--- 72-edit-distance/72-edit-distance.cpp
+++ 72-edit-distance/72-edit-distance.cpp
@@ -25,12 +25,88 @@ public:
         
         return dp[i][j] = ans;
     }
+    
+    int solveTab(string &word1, string &word2){
+        int l1= word1.length();
+        int l2 = word2.length();
+        
+        vector<vector<int>> dp(l1+1, vector<int>(l2+1,0));
+        int ans = 0 ;
+        
+        for(int j = 0; j<l2; j++){
+            dp[l1][j] = l2 - j;
+        }
+        for(int i = 0; i<l1; i++){
+          dp[i][l2] = l1 - i;  
+        }
+        
+        for(int i = l1 -1; i>=0; i--){
+            for(int j = l2-1; j>= 0 ; j--){
+                if(word1[i] == word2[j]){
+                    ans = dp[i+1][j+1];
+                }
+                else{
+                    int inAns = 1+ dp[i][j+1];
+                    int delAns = 1+ dp[i+1][j];
+                    int repAns = 1+ dp[i+1][j+1];
+                    ans = min(inAns, min(delAns, repAns));
+                }
+                dp[i][j] = ans;
+            }
+        }
+        return dp[0][0];
+    }
+    
+    int solveSpace(string &word1, string &word2){
+        int l1= word1.length();
+        int l2 = word2.length();
+        
+        //vector<vector<int>> dp(l1+1, vector<int>(l2+1,0));
+        vector<int>curr(l2+1,0);
+        vector<int>next(l2+1,0);
+        
+        int ans = 0 ;
+        
+        for(int j = 0; j<l2; j++){
+            next[j] = l2 - j;
+        }
+
+        for(int i = l1 -1; i>=0; i--){
+            for(int j = l2-1; j>= 0 ; j--){
+                //Important 
+                curr[l2] = l1 - i;
+                
+                if(word1[i] == word2[j]){
+                    ans = next[j+1];
+                }
+                else{
+                    int inAns = 1+ curr[j+1];
+                    int delAns = 1+ next[j];
+                    int repAns = 1+ next[j+1];
+                    ans = min(inAns, min(delAns, repAns));
+                }
+                curr[j] = ans;
+            }
+            next = curr;
+        }
+        return next[0];
+    }
+    
+    
     int minDistance(string word1, string word2) {
         
         int l1= word1.length();
         int l2 = word2.length();
         
-        vector<vector<int>> dp(l1+1, vector<int>(l2+1,-1));
-        return solve(word1,word2, 0, 0,dp);
+//         vector<vector<int>> dp(l1+1, vector<int>(l2+1,-1));
+//         return solve(word1,word2, 0, 0,dp);
+        //return solveTab(word1, word2);
+        
+        if(l1 == 0)
+            return l2;
+        if(l2 == 0)
+            return l1;
+        
+        return solveSpace(word1, word2);
     }
 };

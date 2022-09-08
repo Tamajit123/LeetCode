0c58a49ead855b474667dbc548cbffaa4a2dea5f
Tamajit123 <85442393+Tamajit123@users.noreply.github.com>
2022-09-09 02:58:02 +0530

    Time: 148 ms (25.44%), Space: 28 MB (7.79%) - LeetHub

--- 44-wildcard-matching/44-wildcard-matching.cpp
+++ 44-wildcard-matching/44-wildcard-matching.cpp
@@ -30,8 +30,40 @@ public:
         return false;
     }
     
+    
+    bool solveTab(string &str, string &pattern){
+        vector<vector<int>> dp(str.length()+1, vector<int>(pattern.length()+1, 0));
+        
+        dp[0][0] = true;
+        
+        for(int j = 1; j<= pattern.length(); j++){
+            bool flag = true;
+            for(int k = 1 ; k <=j; k++){
+                if( pattern[k-1] != '*'){
+                    flag = false;
+                    break;
+            }
+            }
+            dp[0][j] = flag;
+        }
+        for(int i = 1; i<= str.length() ; i++ ){
+            for(int j = 1 ; j<= pattern.length() ; j++){
+                if(str[i-1] == pattern[j-1] || pattern[j-1] == '?')
+                     dp[i][j] = (dp[i-1][j-1]);
+                
+                else if(pattern[j-1] == '*')
+                    dp[i][j] = (dp[i-1][j] || dp[i][j-1]);
+                
+                else 
+                     dp[i][j] = false;
+            }
+        }
+        return dp[str.length()][pattern.length()];
+    }
+    
     bool isMatch(string s, string p) {
-        vector<vector<int>> dp(s.length(), vector<int>(p.length(), -1));
-      return solve(s,p, s.length() - 1, p.length() - 1,dp);  
+      //   vector<vector<int>> dp(s.length(), vector<int>(p.length(), -1));
+      // return solve(s,p, s.length() - 1, p.length() - 1,dp); 
+        return solveTab(s, p);
     }
 };
